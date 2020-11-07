1824afe1b935b30b9b96e63cef414443c5806df1
wshawk <1242293215@qq.com>
2020-11-08 00:40:45 +0800

    update

--- src/main/java/com/companystaffmanage/login/controller/LoginController.java
+++ src/main/java/com/companystaffmanage/login/controller/LoginController.java
@@ -1,7 +1,14 @@
 package com.companystaffmanage.login.controller;
 
+import com.companystaffmanage.common.entity.TestUser;
+import com.companystaffmanage.common.response.R;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author wsHawk
@@ -14,7 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @GetMapping("/hello")
-    public String testLogin(){
-        return "restart";
+    public R testLogin(){
+        List<TestUser> list = new ArrayList<>(11);
+       for (int i=0; i<10; i++){
+           TestUser testUser = new TestUser();
+           testUser.setAge(10+i);
+           testUser.setEmail("test@qq.com");
+           testUser.setId(152L+i);
+           testUser.setName("test");
+           list.add(testUser);
+       }
+//        Map<String,Object> res = new HashMap<>();
+//        res.put("testUser",testUser);
+        return R.success(list);
     }
 }
