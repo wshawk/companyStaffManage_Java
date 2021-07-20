2ae48457c28c34dfbe5bf741e37ac896a11fca7b
wshawk <1242293215@qq.com>
2021-07-20 23:36:56 +0800

    初始化

---
+++ company-staff-manage/src/main/java/com/hawk/company_staff_manage/CompanyStaffManageApplication.java
@@ -0,0 +1,13 @@
+package com.hawk.company_staff_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyStaffManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyStaffManageApplication.class, args);
+    }
+
+}
---
+++ company-staff-manage/src/test/java/com/hawk/company_staff_manage/CompanyStaffManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawk.company_staff_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyStaffManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
