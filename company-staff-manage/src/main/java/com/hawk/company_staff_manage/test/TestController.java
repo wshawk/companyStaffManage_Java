5d9f5b5eb2edab5d2b9fdf1cbf219bef01181119
wshawk <1242293215@qq.com>
2021-07-21 22:47:06 +0800

    增加log4j2打印日志

--- company-staff-manage/src/main/java/com/hawk/company_staff_manage/test/TestController.java
+++ company-staff-manage/src/main/java/com/hawk/company_staff_manage/test/TestController.java
@@ -2,6 +2,7 @@ package com.hawk.company_staff_manage.test;
 
 import com.hawk.company_staff_manage.common.R;
 import com.hawk.company_staff_manage.test.service.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import javax.annotation.Resource;
  * @Description: 测试类
  * @since 2021/7/21 0:10
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -22,6 +24,7 @@ public class TestController {
 
     @RequestMapping("/get_count")
     public R<Integer> getStaffCount(){
+        log.info("TestController getStaffCount Method start");
         return R.success(testService.count());
     }
 
