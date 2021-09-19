50afe9933be1d6948e22070162f93dc98165c9c1
wshawk <1242293215@qq.com>
2021-09-20 00:43:58 +0800

    feat: update

--- company-staff-manage/src/main/java/com/hawk/company_staff_manage/common/GlobalExceptionHandler.java
+++ company-staff-manage/src/main/java/com/hawk/company_staff_manage/common/GlobalExceptionHandler.java
@@ -2,15 +2,11 @@ package com.hawk.company_staff_manage.common;
 
 import com.hawk.company_staff_manage.common.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -25,35 +21,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BizException.class)
     @ResponseBody
-    public <T> R<T> bizExceptionHandler(HttpServletRequest req, BizException e) {
-        log.error("业务异常| 请求路径:{}, 错误码: {}, 提示信息: {}", req.getRequestURI(), e.getErrorCode(), e.getErrorMsg());
+    public <T> R<T> bizExceptionHandler( BizException e) {
+        log.error("业务异常| 错误码: {}, 提示信息: {}", e.getErrorCode(), e.getErrorMsg());
         return R.fail(e.getErrorCode(), e.getErrorMsg());
     }
 
     /**
      * 空指针异常处理程序
      *
-     * @param req HttpServletRequest
      * @param e   e
-     * @return {@link R}
+     * @return {@link R<T>}
      */
     @ExceptionHandler(value = NullPointerException.class)
     @ResponseBody
-    public <T> R<T> exceptionHandler(HttpServletRequest req, NullPointerException e) {
-        log.error("空指针异常,请求路径：{}, 可能出错的位置: {}",req.getRequestURI(), e.getCause().toString());
+    public <T> R<T> exceptionHandler(NullPointerException e) {
+        log.error("空指针异常, 可能出错的位置: {}", e.getCause().toString());
         return R.fail(RP.NULL_POINTER_ERROR);
     }
     /**
      * 其他异常处理程序
      *
-     * @param req HttpServletRequest
      * @param e   e
-     * @return {@link R<Object>}
+     * @return {@link R<T>}
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public <T> R<T> exceptionHandler(HttpServletRequest req, Exception e) {
-        log.error("系统错误, 请求路径: {}, 提示信息: {}, 可能出错的位置: {}",req.getRequestURI(), e.getMessage(), e.getCause().toString());
+    public <T> R<T> exceptionHandler(Exception e) {
+         log.error("系统错误: ", e);
         if (e.getCause() instanceof  BizException){
             BizException bizException = (BizException) e.getCause();
             log.error("业务异常| 错误码: {}, 提示信息: {}",   bizException.getErrorCode(), bizException.getErrorMsg());
